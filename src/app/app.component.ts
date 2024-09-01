a61c9591691077ff48072ff57ff98b0983264d7c
405596-MorenoTobias <405596@tecnicatura.frc.utn.edu.ar>
2024-09-01 19:01:24 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [SobreNosotrosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
